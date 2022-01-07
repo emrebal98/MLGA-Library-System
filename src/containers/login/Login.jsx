718817d15b4c2af5b7f288b3ae62baf477d8e1e2
Berkakbulbul <berkxp@gmail.com>
2022-01-07 23:58:08 +0300

    Login page front

--- src/containers/login/Login.jsx
+++ src/containers/login/Login.jsx
@@ -2,8 +2,82 @@ import React, { useState } from "react";
 import { useNavigate /* Navigate, useLocation */ } from "react-router-dom";
 // import useAuth from "../../useAuth";
 import "./login.css";
+import FormInput from "./FormInput";
+
+
 
 function Login() {
+	const [values, setValues] = useState({
+	  email: "",
+	  password: "",
+	  confirmPassword: "",
+	});
+  
+	const inputs = [
+
+	  {
+		id: 1,
+		name: "email",
+		type: "email",
+		placeholder: "Email",
+		errorMessage: "It should be a valid email address!",
+		label: "Email",
+		required: true,
+	  },
+	  {
+		id: 2,
+		name: "password",
+		type: "password",
+		placeholder: "Password",
+		errorMessage:
+		  "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
+		label: "Password",
+		pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+		required: true,
+	  },
+	  {
+		id: 3,
+		name: "confirmPassword",
+		type: "password",
+		placeholder: "Confirm Password",
+		errorMessage: "Passwords don't match!",
+		label: "Confirm Password",
+		pattern: values.password,
+		required: true,
+	  },
+	];
+  
+	const handleSubmit = (e) => {
+	  e.preventDefault();
+	};
+  
+	const onChange = (e) => {
+	  setValues({ ...values, [e.target.name]: e.target.value });
+	};
+  
+	return (
+	  <div className="app">
+		<form onSubmit={handleSubmit}>
+		  <h1>Medipol Library System</h1>
+		  {inputs.map((input) => (
+			<FormInput
+			  key={input.id}
+			  {...input}
+			  value={values[input.name]}
+			  onChange={onChange}
+			/>
+		  ))}
+		  <button>Login</button>
+		</form>
+	  </div>
+	);
+  };
+
+
+
+/*
+
+{
 	const navigate = useNavigate();
 	// const { authed, login } = useAuth();
 	// const { state } = useLocation();
@@ -71,5 +145,8 @@ function Login() {
 		</div>
 	);
 }
+
+ */
+
 
 export default Login;

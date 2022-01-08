cd13102661a2e985d02a8f6be58ce62da291de83
Emre BAL <59164130+emrebal98@users.noreply.github.com>
2022-01-08 12:52:50 +0300

    css fixed

--- src/containers/login/Login.jsx
+++ src/containers/login/Login.jsx
@@ -5,61 +5,64 @@ import "./login.css";
 import FormInput from "./FormInput";
 
 function Login() {
-	const [values, setValues] = useState({
-	  email: "",
-	  password: "",
-	  confirmPassword: "",
-	});
-  
-	const inputs = [
+	const navigate = useNavigate();
 
-	  {
-		id: 1,
-		name: "email",
-		type: "email",
-		placeholder: "Email",
-		errorMessage: "It should be a valid email address!",
-		label: "Email",
-		required: true,
-	  },
-	  {
-		id: 2,
-		name: "password",
-		type: "password",
-		placeholder: "Password",
-		errorMessage:
-		  "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
-		label: "Password",
-		pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
-		required: true,
-	  }
+	const [values, setValues] = useState({
+		email: "test@test.test",
+		password: "1234Aa@!",
+		confirmPassword: "",
+	});
+
+	const inputs = [
+		{
+			id: 1,
+			name: "email",
+			type: "email",
+			placeholder: "Email",
+			errorMessage: "It should be a valid email address!",
+			label: "Email",
+			required: true,
+		},
+		{
+			id: 2,
+			name: "password",
+			type: "password",
+			placeholder: "Password",
+			errorMessage:
+				"Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
+			label: "Password",
+			pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+			required: true,
+		},
 	];
-  
+
 	const handleSubmit = (e) => {
-	  e.preventDefault();
+		e.preventDefault();
+		console.log("asd");
+		navigate("/home");
 	};
-  
+
 	const onChange = (e) => {
-	  setValues({ ...values, [e.target.name]: e.target.value });
+		setValues({ ...values, [e.target.name]: e.target.value });
 	};
-  
+
 	return (
-	  <div className="app">
-		<form onSubmit={handleSubmit}>
-		  <h1>Make Libraries Great Again</h1>
-		  {inputs.map((input) => (
-			<FormInput
-			  key={input.id}
-			  {...input}
-			  value={values[input.name]}
-			  onChange={onChange}
-			/>
-		  ))}
-		  <button>Login</button>
-		</form>
-	  </div>
+		<div className="login_area">
+			<form onSubmit={handleSubmit}>
+				<h1>Make Libraries Great Again</h1>
+				{inputs.map((input) => (
+					<FormInput
+						key={input.id}
+						{...input}
+						value={values[input.name]}
+						onChange={onChange}
+					/>
+				))}
+				<button>Login</button>
+			</form>
+		</div>
 	);
-  };
+}
 
 /*
 
@@ -133,6 +136,5 @@ function Login() {
 }
 
  */
-
 
 export default Login;
